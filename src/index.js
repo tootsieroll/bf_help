6247461e1644ed18ba04ea614f54c44b62a56274
dina <islamova@ians.aero>
2022-08-21 14:47:19 +0300

    changes after merge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import "./styles/reset.scss";
+import './styles/reset.scss';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-import "./styles/global.scss";
-
+import './styles/global.scss';
 
 window.$ = require('jquery');
 window.slick = require('slick-carousel');
 
-import "./js/main.js";
+import './js/main.js';
